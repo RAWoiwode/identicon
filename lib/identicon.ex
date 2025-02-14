a043744278f62e7c1e8fae09646c4f407bc95839
Ralph Woiwode <rawoiwode@gmail.com>
2025-02-14 09:35:50 -0600

    Using Pattern Matching to grab the RGB values for the random color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -15,6 +15,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
   @doc """
@@ -23,6 +24,14 @@ defmodule Identicon do
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
-    %Identicon.Image{hex: hex}
+
+    %Identicon.Image{hex: hex} # Struct
+  end
+
+  def pick_color(image) do
+    # Pattern match
+    # We are looking for an Image, that has a hex prop, that is a list
+    %Identicon.Image{hex: [r, g, b | _tail]} = image # Only want the first 3, acknowledge the rest but don't use
+    [r, g, b]
   end
 end
