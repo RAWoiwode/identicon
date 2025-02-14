09eac5424396080cb607c058f08a04fd22a40bc7
Ralph Woiwode <rawoiwode@gmail.com>
2025-02-14 09:19:38 -0600

    Adding base documentation for module

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,6 +1,15 @@
 defmodule Identicon do
   @moduledoc """
   Documentation for `Identicon`.
+  An Identicon is
+  * 5x5 grid
+  * 250px tall and wide
+  * Each square is 50x50px
+  * Random squares are colored
+  * One random color used throughout
+  * Vertically symmetrical
+  * Each itdenticon should be based on a string input i.e. "billy" will always result
+    in the same identicon
   """
 
   def main(input) do
